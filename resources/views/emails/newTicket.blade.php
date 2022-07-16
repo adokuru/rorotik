9937a5a27f0a8c225a09c4197ff1929b092799c6
David Adokuru <davidadokuru@gmail.com>
2022-07-16 10:30:26 +0100

    Circle creation

--- resources/views/emails/newTicket.blade.php
+++ resources/views/emails/newTicket.blade.php
@@ -1,4 +1,4 @@
-@component('mail::message')
+<div>
 Introduction
 
 The body of your message.
@@ -6,7 +6,6 @@ The body of your message.
  $qrCodeAsPng = QrCode::format('png')->size(500)->generate("my text for the QR code");
 ?>
 <img src="{{ $message->embedData($qrCodeAsPng, 'nameForAttachment.png') }}" />
-
+</div>
 Thanks,<br>
 {{ config('app.name') }}
-@endcomponent
