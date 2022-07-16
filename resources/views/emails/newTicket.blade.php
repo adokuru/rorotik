6fbc5d5b3d9afa137941050b264dfbd906eae3ae
David Adokuru <davidadokuru@gmail.com>
2022-07-16 10:25:46 +0100

    Circle creation

--- resources/views/emails/newTicket.blade.php
+++ resources/views/emails/newTicket.blade.php
@@ -3,7 +3,7 @@
 
 The body of your message.
 
-<img src="{!!$message->embedData($details['qr'], 'QrCode.png', 'image/png')!!}">
+<img src="{{ $message->embedData($details['qr'], 'nameForAttachment.png') }}" />
 
 Thanks,<br>
 {{ config('app.name') }}
